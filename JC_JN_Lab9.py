2fdeab6cd957da4234767a5e884b3ad59757b29d
javi2004No <102776054+javi2004No@users.noreply.github.com>
2024-04-11 15:28:17 -0400

    Added the decoding function and changed the menu

--- JC_JN_Lab9.py
+++ JC_JN_Lab9.py
@@ -1,12 +1,27 @@
-# Jessica Chao
+def decode_password(password):
+    """
+    Decodes the password by subtracting three to each number.
+    :param password: The encoded password.
+    :return: the decoded password or an error if the password is not all numerical.
+    """
+    newPassword = ""
+    for num in password:
+        holder = int(num) - 3
+        # if the password is negative adds 10 to get the shift.
+        if holder < 0:
+            holder += 10
+        newPassword += str(holder)
+    return newPassword
 
+
+# Jessica Chao
 def encode_password(password):
     encodepass = ''.join(str((int(digit) + 3) % 10) for digit in password)
     return encodepass
 # Jess-created the encode function to start the project so that the str of numbers can be encoded
-
+done = ""
 while True:
-    print("Menu")
+    print("\nMenu")
     print("-------------")
     print("1. Encode")
     print("2. Decode")
@@ -14,7 +29,7 @@ while True:
 
 # Jess-while loop started to display the menu
 
-    choice = input("Please enter an option: ")
+    choice = int(input("\nPlease enter an option: "))
 
 # Jess-created a choice for the user to input their option
 
@@ -22,6 +37,10 @@ while True:
         password = input("Please enter your password to encode: ")
         done = encode_password(password)
         print("Your password has been encoded and stored!")
+    elif choice == 2:
+        # Added this function call Javier Noda
+        decoded = decode_password(done)
+        print(f"The encoded password is {done}, and the original password is {decoded}.")
 # Jess-when choice 1 is selected the function encode_password is used to encode the str
 
     elif choice == 3:
